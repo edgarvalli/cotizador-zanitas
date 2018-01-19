84f5401fa84e120779468989f8e075070a84f37c
edgarvalli <edgarvalli80@gmail.com>
2018-01-19 16:05:20 -0600

    Set a constructor on Quote Class

--- api/cotizador/quotes/quotes.class.js
+++ api/cotizador/quotes/quotes.class.js
@@ -7,6 +7,10 @@ const db = "quotes";
 
 class Quote {
 
+    constructor() {
+        this.limit = 50
+    }
+
     fetch(req,res) {
 
         const querys = {
@@ -14,7 +18,8 @@ class Quote {
                 name: 1,
                 phone: 1,
                 total: 1,
-            }
+            },
+            limit: this.limit
         }
 
         mongo(db).find(querys, (err, docs) => err ? console.log(err) : res.send(docs))
@@ -49,6 +54,12 @@ class Quote {
                 res.json(data)
             }
         })
+    }
+
+    getMore(req, res) {
+
+
+
     }
 
     formatDate(date) {
